5361c81a26d9cc094f3e78c2130817ca582e54d0
DavidAda04 <127976670+DavidAda04@users.noreply.github.com>
2024-10-03 17:23:32 -0400

    Update InsertionSort.test.js

--- src/algorithms/sorting/insertion-sort/__test__/InsertionSort.test.js
+++ src/algorithms/sorting/insertion-sort/__test__/InsertionSort.test.js
@@ -1,11 +1,11 @@
-import InsertionSort from '../InsertionSort';
+import Sorter from '../Optimise_Insertion_Sort'; // Import the Sorter class
 import {
   equalArr,
   notSortedArr,
   reverseArr,
   sortedArr,
   SortTester,
-} from '../../SortTester';
+} from '../../SortTester'; // Assuming these are helper methods
 
 // Complexity constants.
 const SORTED_ARRAY_VISITING_COUNT = 19;
@@ -13,50 +13,69 @@ const NOT_SORTED_ARRAY_VISITING_COUNT = 100;
 const REVERSE_SORTED_ARRAY_VISITING_COUNT = 209;
 const EQUAL_ARRAY_VISITING_COUNT = 19;
 
-describe('InsertionSort', () => {
+// Setup comparator and callbacks for the Sorter
+const callbacks = {
+  visitingCallback: (value) => {
+    // Optionally, you can log or track visited elements here
+  },
+};
+
+const comparator = {
+  lessThan: (a, b) => a < b, // Custom comparator for less than
+};
+
+describe('Sorter', () => {
   it('should sort array', () => {
-    SortTester.testSort(InsertionSort);
+    const sorter = new Sorter(callbacks, comparator);
+    SortTester.testSort(sorter); // Assuming testSort uses the class with a sort method
   });
 
   it('should sort array with custom comparator', () => {
-    SortTester.testSortWithCustomComparator(InsertionSort);
+    const sorter = new Sorter(callbacks, comparator);
+    SortTester.testSortWithCustomComparator(sorter);
   });
 
   it('should do stable sorting', () => {
-    SortTester.testSortStability(InsertionSort);
+    const sorter = new Sorter(callbacks, comparator);
+    SortTester.testSortStability(sorter);
   });
 
   it('should sort negative numbers', () => {
-    SortTester.testNegativeNumbersSort(InsertionSort);
+    const sorter = new Sorter(callbacks, comparator);
+    SortTester.testNegativeNumbersSort(sorter);
   });
 
   it('should visit EQUAL array element specified number of times', () => {
+    const sorter = new Sorter(callbacks, comparator);
     SortTester.testAlgorithmTimeComplexity(
-      InsertionSort,
+      sorter,
       equalArr,
       EQUAL_ARRAY_VISITING_COUNT,
     );
   });
 
   it('should visit SORTED array element specified number of times', () => {
+    const sorter = new Sorter(callbacks, comparator);
     SortTester.testAlgorithmTimeComplexity(
-      InsertionSort,
+      sorter,
       sortedArr,
       SORTED_ARRAY_VISITING_COUNT,
     );
   });
 
   it('should visit NOT SORTED array element specified number of times', () => {
+    const sorter = new Sorter(callbacks, comparator);
     SortTester.testAlgorithmTimeComplexity(
-      InsertionSort,
+      sorter,
       notSortedArr,
       NOT_SORTED_ARRAY_VISITING_COUNT,
     );
   });
 
   it('should visit REVERSE SORTED array element specified number of times', () => {
+    const sorter = new Sorter(callbacks, comparator);
     SortTester.testAlgorithmTimeComplexity(
-      InsertionSort,
+      sorter,
       reverseArr,
       REVERSE_SORTED_ARRAY_VISITING_COUNT,
     );
